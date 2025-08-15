6d3a85815b8e223df1df47bb2a1ca6748cede39c
OrangGanteng <49611257+OrangGanteng@users.noreply.github.com>
2025-08-15 23:49:08 +0700

    Update LKS-SHIELD-V1.ino

--- LKS-SHIELD-V1.ino
+++ LKS-SHIELD-V1.ino
@@ -1,30 +1,12 @@
-#include <AFMotor.h>
+#define IR_USE_AVR_TIMER1
 #include <IRremote.hpp>
-#define IR_RECEIVE_PIN 9
+#include <AFMotor.h>
+#define IR_RECEIVE_PIN A0
 
-AF_DCMotor motor1(3);
-AF_DCMotor motor2(4);
-AF_DCMotor motor3(5);
-AF_DCMotor motor4(6);
-
-int kode_maju = ;
-int kode_mundur = ;
-int kode_kiri = ;
-int kode_kanan = ;
-
-void maju(){
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
-}
-
-void mundur(){
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
-}
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
 
 void kanan(){
   motor1.run(FORWARD);
@@ -46,40 +28,58 @@ void berhenti(){
   motor3.run(RELEASE);
   motor4.run(RELEASE);
 }
-void setup(){
-  IrReceiver.begin(IR_RECEIVE_PIN);
-  
-  motor1.setSpeed(100);
-  motor2.setSpeed(100);
-  motor3.setSpeed(100);
-  motor4.setSpeed(100);
 
-  motor1.run(RELEASE);
-  motor2.run(RELEASE);
-  motor3.run(RELEASE);
-  motor4.run(RELEASE);
+void maju(){
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
 }
 
-void loop(){
+void mundur(){
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}
+
+ 
+void setup() {
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN);
+
+  motor1.setSpeed(120);
+  motor2.setSpeed(120);
+  motor3.setSpeed(120);
+  motor4.setSpeed(120); 
+}
+
+void loop() {
   if (IrReceiver.decode()) {
     uint16_t command = IrReceiver.decodedIRData.command;
     Serial.println(command);
     delay(100);  // wait a bit
-    switch (IrReceiver.decodedIRData.command){
-      case kode_maju:
-        maju();
-        break;
-      case kode_mundur:
-        mundur();
-        break;
-      case kode_kiri:
-        kiri();
-        break;
-      case kode_kanan:
-        kanan();
-        break;
-      default:
-        berhenti();
-    }
     IrReceiver.resume();
+    if (command == 24) {
+      Serial.println("Maju");
+      motor1.run(FORWARD);
+      motor2.run(FORWARD);
+      motor3.run(FORWARD);
+      motor4.run(FORWARD);
+      
+    } else if (command == 28) {
+      Serial.println("Berhenti");
+      motor1.run(RELEASE);
+      motor2.run(RELEASE);
+      motor3.run(RELEASE);
+      motor4.run(RELEASE);
+      
+    } else if (command == 82) {
+      Serial.println("Mundur");
+      motor1.run(BACKWARD);
+      motor2.run(BACKWARD);
+      motor3.run(BACKWARD);
+      motor4.run(BACKWARD);
+    }
+  }
 }
